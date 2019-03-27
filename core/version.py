7c8cca3ebac2083c4afa7d656753a723c380baba
interkosmos <pengel@hs-nb.de>
2019-03-27 21:13:17 +0100

    Clean-up.

--- core/version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3.6
-
-__author__ = 'Philipp Engel'
-__copyright__ = 'Copyright (c) 2017 Hochschule Neubrandenburg'
-__license__ = 'BSD-2-Clause'
-
-APP_NAME = 'OpenADMS Server'
-APP_VERSION = '0.1'
-APP_API = 'v1'
