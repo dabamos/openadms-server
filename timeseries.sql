4a7ce4c5556d7d2fccdbc9e2f9ec04f91cdc1877
Philipp <pengel@hs-nb.de>
2020-03-21 18:56:33 +0100

    Added log messages table.

--- timeseries.sql
+++ timeseries.sql
@@ -1,5 +1,6 @@
 --
--- PostgreSQL database table for observation data and sensor node pings.
+-- PostgreSQL database table for observation data, log messages, and sensor node
+-- pings.
 --
 -- Date:    2020-03-20
 -- Author:  Philipp Engel
@@ -8,6 +9,7 @@
 BEGIN;
 
 SET CLIENT_ENCODING = 'UTF8';
+SET TIMEZONE = 'Europe/Berlin';
 
 CREATE SCHEMA openadms;
 
@@ -27,16 +29,34 @@ CREATE TABLE openadms.heartbeats (
     UNIQUE (pid, nid)
 );
 
--- Create indices.
-CREATE INDEX idx_id        ON openadms.observations ((data->>'id'));
-CREATE INDEX idx_nid       ON openadms.observations ((data->>'nid'));
-CREATE INDEX idx_pid       ON openadms.observations ((data->>'pid'));
-CREATE INDEX idx_sensor    ON openadms.observations ((data->>'sensorName'));
-CREATE INDEX idx_target    ON openadms.observations ((data->>'target'));
-CREATE INDEX idx_timestamp ON openadms.observations ((data->>'timestamp'));
-CREATE INDEX idx_type      ON openadms.observations ((data->>'type'));
+-- Create the log messages table.
+CREATE TABLE openadms.logs (
+    id      BIGSERIAL PRIMARY KEY,
+    pid     VARCHAR(80)  NOT NULL,
+    nid     VARCHAR(80)  NOT NULL,
+    dt      TIMESTAMPTZ,
+    module  VARCHAR(80)  NOT NULL,
+    level   VARCHAR(8)   NOT NULL,
+    message VARCHAR(250) NOT NULL
+);
+
+-- Create additional indices for table `openadms.observations`.
+CREATE INDEX idx_obs_id        ON openadms.observations ((data->>'id'));
+CREATE INDEX idx_obs_nid       ON openadms.observations ((data->>'nid'));
+CREATE INDEX idx_obs_pid       ON openadms.observations ((data->>'pid'));
+CREATE INDEX idx_obs_sensor    ON openadms.observations ((data->>'sensorName'));
+CREATE INDEX idx_obs_target    ON openadms.observations ((data->>'target'));
+CREATE INDEX idx_obs_timestamp ON openadms.observations ((data->>'timestamp'));
+CREATE INDEX idx_obs_type      ON openadms.observations ((data->>'type'));
+
+-- Create additional indices for table `openadms.logs`.
+CREATE INDEX idx_log_dt    ON openadms.logs (dt);
+CREATE INDEX idx_log_level ON openadms.logs (level);
+CREATE INDEX idx_log_nid   ON openadms.logs (nid);
+CREATE INDEX idx_log_pid   ON openadms.logs (pid);
 
 COMMIT;
 
 ANALYZE openadms.observations;
 ANALYZE openadms.heartbeats;
+ANALYZE openadms.logs;
